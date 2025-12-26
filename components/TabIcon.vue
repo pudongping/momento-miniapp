de14122a431cdffd4c721b79c785d5c8a4897549
Alex Pu <276558492@qq.com>
2025-12-27 03:30:42 +0800

    tabbar图标

---
+++ components/TabIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <view class="tab-icon-container">
+    <uni-icons :type="type" :color="color" size="24"></uni-icons>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'TabIcon',
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    color: {
+      type: String,
+      default: '#666666'
+    }
+  }
+}
+</script>
+
+<style>
+.tab-icon-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
